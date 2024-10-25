dee1526886cb5fc9f477ef3a91002cebb98d980a
Nikhil Biby <nikhilbiby9876@gmail.com>
2024-10-25 23:14:49 +0530

    Created all required websites
    All websites are created by modifying base.html as required.

--- ArduinoIDE/Clock.ino/Clock.ino.ino
+++ ArduinoIDE/Clock.ino/Clock.ino.ino
@@ -5,13 +5,19 @@
 const char* ssid = "Clock";
 IPAddress apIP(192, 168, 1, 1);
 ESP8266WebServer server(80);
+const int rs = D0, en = D1, d4 = D2, d5 = D3, d6 = D4, d7 = D5;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
   WiFi.softAP(ssid);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  lcd.begin(16, 1);
+  lcd.setCursor(0, 0);
+  lcd.print("1234567891234567");
+  lcd.setCursor(0, 1);
+  lcd.print("1234567890123456");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
